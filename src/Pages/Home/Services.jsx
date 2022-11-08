2097fe64434bc1a8cf46bc1770597f76ed6d0a47
Nightowlam <nightowl3.00am@gmail.com>
2022-11-08 16:39:22 +0600

    footer added

--- src/Pages/Home/Services.jsx
+++ src/Pages/Home/Services.jsx
@@ -14,14 +14,15 @@ const Services = () => {
   }, []);
   return (
     <div>
-      <div className="grid grid-cols-3 gap-6">
+              <h2 className="text-center text-3xl font-bold mb-10 text-red-600 underline">Services</h2>
+          <div className="grid grid-cols-3 gap-10 w-[90%] mx-auto">
         {services?.map((service) => (
           <Service key={service._id} service={service}></Service>
         ))}
       </div>
       <div className="text-center mb-28 mt-12">
-        <Link to='/services'>
-          <button className="border border-black px-6 mt-3 py-1.5 translate duration-500 ease-in  hover:bg-red-600 hover:text-white">
+        <Link to="/services">
+          <button className="border border-red-600 text-red-500 font-bold px-6 mt-3 py-1.5 translate duration-500 ease-in  hover:bg-red-600 hover:text-white">
             See more
           </button>
         </Link>
