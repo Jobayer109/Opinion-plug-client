c573cc92ff3609c8be459f5ecfd377426e5de548
Nightowlam <nightowl3.00am@gmail.com>
2022-11-08 17:01:32 +0600

    error page created

--- src/Pages/Shared/ErrorPage.jsx
+++ src/Pages/Shared/ErrorPage.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import { FaGrimace } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <section className="flex items-center sm:p-16 bg-black h-screen">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8 space-y-8 text-center sm:max-w-md">
+        <FaGrimace className="text-red-600 text-6xl" />
+        <p className="text-3xl text-gray-400"> Ops.!! I thing you forget your route.</p>
+        <Link
+          to="/"
+          rel="noopener noreferrer"
+          className="px-8 py-3 font-semibold rounded bg-red-600 text-white hover:bg-green-500 hover:text-black"
+        >
+          Back to homepage
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default ErrorPage;
